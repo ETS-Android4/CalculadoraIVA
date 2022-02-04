454ae48426abd3a44b107c8f23dcf1f2ca3868a3
Pamela Benavides <pamebengloff@gmail.com>
2022-02-03 19:01:39 -0700

    Added this project in github

---
+++ CalculadoraIVA/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace CalculadoraIVA
+{
+    [Activity(MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        EditText entradaCantidad;
+        Button botonCalcular;
+        TextView resultadoIVA;
+
+       TextView resultadoTotal;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            entradaCantidad = FindViewById<EditText>(Resource.Id.entradaCantidad);
+            botonCalcular = FindViewById<Button>(Resource.Id.botonCalcular);
+            resultadoIVA = FindViewById<TextView>(Resource.Id.resultadoIVA);
+
+           resultadoTotal = FindViewById<TextView>(Resource.Id.resultadoTotal);
+
+            botonCalcular.Click += OnCalcularClick;
+        }
+
+        private void OnCalcularClick(object sender, EventArgs e)
+        {
+            string texto = entradaCantidad.Text;
+            double cantidad;
+            if(double.TryParse(texto, out cantidad))
+            {
+                double IVA = cantidad * 0.16;
+                resultadoIVA.Text = IVA.ToString("C");
+
+                double resultado = cantidad + IVA;
+                resultadoTotal.Text = resultado.ToString("C");
+            }
+
+ 
+            
+        }
+
+    }
+}
---
+++ CalculadoraIVA/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 1266b8a43b3222b3b30b3a71783b7f4a41a35f8e */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1147
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 300515
+	.include	"typemap.mj.inc"
